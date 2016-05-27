1c76d1a130a927ce241b8613ea3496c47af45aa1
CopyAndPasteHub <copyandpastewebsite@gmail.com>
2016-05-27 12:17:36 +0200

    First commit

---
+++ AFB02TextViews/app/src/main/java/website/copyandpaste/afb02textviews/MainActivity.java
@@ -0,0 +1,19 @@
+package website.copyandpaste.afb02textviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //this is MainActivity
+
+    //This is method for creating our app
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Click on sidebar at "layout"
+    //Now we can run our first app!
+    //(in next tutorial)
+}
