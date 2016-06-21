799650113d57928872866e802c1aa0d82ac5dda8
Paweł Marcinkiewicz <pawelmarcinkiewicz0@gmail.com>
2016-06-21 06:53:14 +0200

    Bottom bar navigation

---
+++ Templates/BottomBarNavigationWith3Fragments/app/src/main/java/website/copyandpaste/bottombarnavigationwith3fragments/MainActivity.java
@@ -0,0 +1,89 @@
+package website.copyandpaste.bottombarnavigationwith3fragments;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ncapdevi.fragnav.FragNavController;
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomBar mBottomBar;
+    private FragNavController fragNavController;
+
+    //indices to fragments
+    private final int TAB_FIRST = FragNavController.TAB1;
+    private final int TAB_SECOND = FragNavController.TAB2;
+    private final int TAB_THIRD = FragNavController.TAB3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //FragNav
+        //list of fragments
+        List<Fragment> fragments = new ArrayList<>(3);
+
+        //add fragments to list
+        fragments.add(FirstFragment.newInstance(0));
+        fragments.add(SecondFragment.newInstance(0));
+        fragments.add(ThirdFragment.newInstance(0));
+
+        //link fragments to container
+        fragNavController = new FragNavController(getSupportFragmentManager(),R.id.container,fragments);
+        //End of FragNav
+
+        //BottomBar menu
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItems(R.menu.bottombar_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                //switch between tabs
+                switch (menuItemId) {
+                    case R.id.bottomBarItemOne:
+                        fragNavController.switchTab(TAB_FIRST);
+                        break;
+                    case R.id.bottomBarItemSecond:
+                        fragNavController.switchTab(TAB_SECOND);
+                        break;
+                    case R.id.bottomBarItemThird:
+                        fragNavController.switchTab(TAB_THIRD);
+                        break;
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    fragNavController.clearStack();
+                }
+            }
+        });
+        //End of BottomBar menu
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (fragNavController.getCurrentStack().size() > 1) {
+            fragNavController.pop();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        mBottomBar.onSaveInstanceState(outState);
+    }
+}
